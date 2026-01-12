63a7ce0b2874c9c7468216ff0d82d1e9c48005cd
CyrillKR <88666557+CyrillKR@users.noreply.github.com>
2026-01-12 16:37:05 +0200

    simple paddle collision

--- src/ball.js
+++ src/ball.js
@@ -3,6 +3,8 @@ export default class Ball {
     this.image = document.querySelector("#game-ball");
     this.size = 30;
 
+    this.game = game;
+
     this.gameWidth = game.gameWidth;
     this.gameHeight = game.gameHeight;
 
@@ -21,17 +23,37 @@ export default class Ball {
     this.position.x += this.speed.x;
     this.position.y += this.speed.y;
 
+    // Collision LEFT/RIGHT
     if (
       this.position.x + this.size / 2 > this.gameWidth ||
       this.position.x + this.size / 2 - 1 < 0
     ) {
       this.speed.x = -this.speed.x;
     }
+
+    // Collision TOP/BOTTOM
     if (
       this.position.y + this.size / 2 > this.gameHeight ||
       this.position.y + this.size / 3 < 0
     ) {
       this.speed.y = -this.speed.y;
+    }
+
+    // Collision PADDLE
+    let bottomOfBall = this.position.y + this.size;
+    let topOfPaddle = this.game.paddle.position.y;
+
+    let leftSideOfPaddle = this.game.paddle.position.x;
+    let rightSideOfPaddle =
+      this.game.paddle.position.x + this.game.paddle.width;
+
+    if (
+      bottomOfBall >= topOfPaddle &&
+      this.position.x >= leftSideOfPaddle &&
+      this.position.x <= rightSideOfPaddle
+    ) {
+      this.speed.y = -this.speed.y;
+      this.position.y = this.game.paddle.position.y - this.size;
     }
   }
 
