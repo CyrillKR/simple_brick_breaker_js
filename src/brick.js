f0f00f356d6f936bbf14367b1656664fbe32e0fa
CyrillKR <88666557+CyrillKR@users.noreply.github.com>
2026-01-12 18:22:26 +0200

    added brick class, rendered successfully on the canvas

---
+++ src/brick.js
@@ -0,0 +1,22 @@
+export default class Brick {
+  constructor(game, position) {
+    this.game = game;
+
+    this.image = document.querySelector("#game-brick");
+    this.position = position;
+    this.width = 52;
+    this.height = 24;
+  }
+
+  update() {}
+
+  draw(ctx) {
+    ctx.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+  }
+}
--- src/game.js
+++ src/game.js
@@ -1,5 +1,6 @@
 import Ball from "./ball.js";
 import Paddle from "./paddle.js";
+import Brick from "./brick.js";
 import InputHandler from "./inputHandler.js";
 
 export default class Game {
@@ -12,7 +13,13 @@ export default class Game {
     this.ball = new Ball(this);
     this.paddle = new Paddle(this);
 
-    this.gameObjects = [this.ball, this.paddle];
+    let bricks = [];
+
+    for (let i = 0; i < 10; i++) {
+      bricks.push(new Brick(this, { x: i * 52, y: 30 }));
+    }
+
+    this.gameObjects = [this.ball, this.paddle, ...bricks];
 
     new InputHandler(this.paddle);
   }
